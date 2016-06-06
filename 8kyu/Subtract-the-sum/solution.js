6d0122c06c2a7e444d8bcad23a4b149c4c24f6ef
danmandel <danmandel@users.noreply.github.com>
2016-06-05 21:09:19 -0400

    Create solution.js

---
+++ 8kyu/Subtract-the-sum/solution.js
@@ -0,0 +1,14 @@
+function SubtractSum(n){
+  // 1. sum all the digits of n
+  stringArr = n.toString().split("");
+  digitArr = stringArr.map(x => Number(x));
+  digitArrSum = digitArr.reduce((a,b), a+b);
+  
+  // 2. subtract the sum from n
+  newNumber = n - digitArrSum;
+  
+  // 3. if newNumber is in the list, return the associated fruit. else, go back to 1. 
+  
+ 
+  return ;// fruit name like "apple"
+}
