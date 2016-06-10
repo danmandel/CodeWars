6b91ca8794209bf99b8a48d82cd60588768c34b4
danmandel <danmandel@users.noreply.github.com>
2016-06-10 18:45:19 -0400

    Update solution.js

--- 7kyu/get-sum-with-nested-arrays/solution.js
+++ 7kyu/get-sum-with-nested-arrays/solution.js
@@ -9,3 +9,14 @@ function arraySum(arr) {
   };
   return sum;
 }
+
+// Best practices submission by MMMAAANNN
+function arraySum(arr){
+  var output = 0;
+  for (var i in arr){
+    var item = arr[i];
+    if ( typeof(item)==="number"    ) { output += item;           }
+    if ( item.constructor === Array ) { output += arraySum(item); }
+  }
+  return output;
+}
