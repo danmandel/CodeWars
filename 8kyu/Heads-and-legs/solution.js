29a28b4f5902263f1d3a234ec1a34846f98b2f0b
danmandel <danmandel@users.noreply.github.com>
2016-06-09 22:24:16 -0400

    Update solution.js

--- 8kyu/Heads-and-legs/solution.js
+++ 8kyu/Heads-and-legs/solution.js
@@ -1,31 +1,17 @@
 function animals(heads, legs){
-
-  var numAnimals = []; // chickens, cows
   
-  // create array of all possible answers
-  for (i=0;i<heads+1;i++){
-      numAnimals.push([i, heads-i]); //[[0,72],[1,71],[2,70]...[72,0]] 
-  };
+  var numAnimals = [];
   
-  // 
-  for (i=0;i<heads+1;i++){
-      chickens = numAnimals[i][0];
-      cows = numAnimals[i][1];
-      
-      if (chickens*2+cows*4==legs){
-          return [chickens,cows];
+  //[[0,72],...,[72,0]]
+  for (i=0;i<heads+1;i++){ 
+      if (i*2+(heads-i)*4==legs){
+        numAnimals.push([i, heads-i]); // [chickens, cows]    
       }
-     
   };
   
-  // Edge cases
-  if (legs > heads*4){
+  if (numAnimals.length > 0){
+      return numAnimals[0];
+  } else {
       return "No solutions";
-  };
-  if (heads == 0){
-          return [0,0];
-  };
-  
-  return "No solutions";
- 
+  }
 }
