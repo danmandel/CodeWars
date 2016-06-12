bb49920d07d90c4131697ee8183481c3e141b2f1
danmandel <danmandel@users.noreply.github.com>
2016-06-12 17:10:54 -0400

    Create solution.js

---
+++ 7kyu/determine-if-name/solution.js
@@ -0,0 +1,11 @@
+function showMe(yourID){
+  if (yourID[0] === "-" || yourID[yourID.length-1]==="-"){return false;};
+  if (yourID[0] !== yourID[0].toUpperCase()){return false;};
+  
+  for (var i=0;i<yourID.length;i++){
+      if (yourID[i] === "-"){return true;};
+      if (yourID[i] === " "){return false;};
+      if (typeof(yourID[i]) !== "string"){return false};
+  }
+  return true;
+}
