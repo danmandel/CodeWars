2d074f9f353fc6ece0a1b99a5d98a6177469dc08
danmandel <danmandel@users.noreply.github.com>
2016-06-13 23:12:00 -0400

    Update solution.js

--- 7kyu/every-nth-array-element/solution.js
+++ 7kyu/every-nth-array-element/solution.js
@@ -9,3 +9,12 @@ function every(arr, interval, start){
       return result;
   } 
 }
+
+// best practices submission by nikola1970
+function every(arr, interval, start){
+  var result = [];
+  for (var i=start || 0; i < arr.length; i += interval || 1) {
+      result.push(arr[i]);
+  }
+  return result;
+}
