6fccbacf5289c1c39609211f5dd7b625f334453f
danmandel <danmandel@users.noreply.github.com>
2016-07-17 01:19:39 -0400

    Create solution-using-apply.js

---
+++ 8kyu/max-and-min-of-arr/solution-using-apply.js
@@ -0,0 +1,9 @@
+var min = function(list){
+    var result = Math.min.apply(null, list);
+    return result; 
+}
+
+var max = function(list){
+    var result = Math.max.apply(null,list);
+    return result;
+}
