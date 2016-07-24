c0cc5ec968ae9a22763d19843aa34b47369643ef
danmandel <danmandel@users.noreply.github.com>
2016-07-23 23:56:06 -0400

    Update solution.js

--- 7kyu/mumbling/solution.js
+++ 7kyu/mumbling/solution.js
@@ -2,13 +2,12 @@ function accum(s) {
     var result = '';
     var resultArr = [];
     var charArr = s.split('');
-    var arr = charArr.map(function(letter,i){
+    charArr.map(function(letter,i){
         resultArr.push(letter.repeat(i+1));
     });
-    var arr = resultArr.map(function(word){
-       word[0] = word[0].toUpperCase();// + word.slice(1,word.length);
+    resultArr = resultArr.map(function(word){
+       return word[0].toUpperCase()+word.slice(1,word.length).toLowerCase();
     });
     result = resultArr.join('-');
-    
     return result;
 }
