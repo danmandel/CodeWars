2e77fa69ba22dfcf02ae49c7a34fb8f07810c154
danmandel <danmandel@users.noreply.github.com>
2016-12-22 23:04:48 -0500

    Update solution.js

--- 7kyu/unpacking-args/solution.js
+++ 7kyu/unpacking-args/solution.js
@@ -1,1 +1,1 @@
-const spread = (func, args) => func.apply(null, args);
+const spread = (func, args) => func.apply(null, args); // func.apply(this, args); might be better if spread is called in a different context like though apply/bind/call or by placing it on an object.
