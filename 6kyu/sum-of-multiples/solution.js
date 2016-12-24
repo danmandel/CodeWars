cf49a23edb1f6dc8ff12979df2892cf0ed8b0e2e
danmandel <danmandel@users.noreply.github.com>
2016-12-24 02:52:17 -0500

    Create solution.js

---
+++ 6kyu/sum-of-multiples/solution.js
@@ -0,0 +1,9 @@
+const solution = number => {
+    const multiples = [];
+    for (let i=0;i<number;i++){
+        if (i % 3 === 0) multiples.push(i);
+        if (i % 5 === 0) multiples.push(i);
+    };
+    const [...uniqueMultiples] = new Set(multiples);
+    return uniqueMultiples.reduce((p,c) => p+c,0);
+};
