fa0ee003af6c0818a63be173c95c83563104145d
danmandel <danmandel@users.noreply.github.com>
2017-01-22 23:53:26 -0500

    Create solution.cs

---
+++ 8kyu/max-and-min-of-arr/solution.cs
@@ -0,0 +1,26 @@
+public class Kata
+{
+  public int Min(int[] list)
+  {
+  	int min = list[0];
+
+    for (int i=0;i<list.Length;i++)
+ 		{
+        if (list[i] < min)
+            min = list[i];
+    }
+    return min;
+  }
+  
+  public int Max(int[] list)
+  {
+      int max = list[0];
+         
+      for (int j=0;j<list.Length;j++)
+      {
+          if (list[j] > max)
+              max = list[j];
+      }
+      return max;
+  } 
+}
