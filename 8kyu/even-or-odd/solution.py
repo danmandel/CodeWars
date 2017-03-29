36fc64a6a899b2e0ca3323d4872c215cb2d3bca8
danmandel <danmandel@users.noreply.github.com>
2017-03-28 21:15:41 -0400

    Create solution.py

---
+++ 8kyu/even-or-odd/solution.py
@@ -0,0 +1,10 @@
+def even_or_odd(number):
+   if number % 2 == 0:
+       return 'Even'
+   else:
+       return 'Odd'
+       
+       
+#top rated
+def even_or_odd(number):
+  return 'Even' if number % 2 == 0 else 'Odd'
