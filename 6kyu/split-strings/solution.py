ee69f94c58155a89584a1857c11b168fa0febc13
danmandel <danmandel@users.noreply.github.com>
2017-03-30 21:21:10 -0400

    Update solution.py

--- 6kyu/split-strings/solution.py
+++ 6kyu/split-strings/solution.py
@@ -2,12 +2,11 @@ import re
 def solution(s):
     regex = re.compile(r'\w{2}') 
     matches = re.findall(regex, s)
-    if len(s) % 2 == 0:
-        return [x for x in matches] 
-    else:
-        arr = [x for x in matches]
-        arr.append(s[-1:]+'_')
-        return arr 
+    result = [x for x in matches]
+    if len(s) % 2 != 0:
+        result.append(s[-1:]+'_')
+    return result
 
 
-#top rated
+def solution(s):
+    return [s[x:x+2] if x < len(s) - 1 else s[-1] + "_" for x in range(0, len(s), 2)]
