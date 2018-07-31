990951092cbb38e2320cf1411f7990c3e852715d
Dan Mandel <dan19926@gmail.com>
2018-07-30 23:56:43 -0400

    solved

---
+++ 8kyu/Conor-McGregor-vs-George-Saint-Pierre/solution.js
@@ -0,0 +1,3 @@
+const quote = fighter => fighter.toLowerCase() === 'conor mcgregor' 
+    ? "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+    : "I am not impressed by your performance.";
